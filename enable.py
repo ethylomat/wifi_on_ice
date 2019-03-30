70dc19e66f762baa974e96de499bf979f2f7794a
Lennart Stipulkowski <stipulkowski@stud.uni-heidelberg.de>
2019-03-30 15:12:50 +0100

    Making tool more lightweigt (Robobrowser/Requests) and automating reconnection

--- enable.py
+++ enable.py
@@ -3,22 +3,17 @@ import argparse
 import os
 import signal
 import sys
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
-from time import sleep
+import time
+
+from robobrowser import RoboBrowser
+import requests
+from wireless import Wireless
 
 
 parser = argparse.ArgumentParser(description='Enables unlimited WiFi Data at the Deutsch Bahn ICE WiFi network')
 parser.add_argument('-I', '--interface', type=str, help='defines the wlan interface to use', default='wlan0')
 parser.add_argument('-s', '--sleep', type=int, help='sleep n second(s) beetween each update', default=30)
-parser.add_argument('--firefox-options', type=str, help='additional arguments to parse to firefox', default=['--headless'], nargs=argparse.REMAINDER)
 args = parser.parse_args()
-
-firefox_options=Options()
-for option in args.firefox_options:
-    firefox_options.add_argument(option)
 
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!')
@@ -26,22 +21,37 @@ def signal_handler(sig, frame):
     print('wifi_on_ice has stopped.')
     sys.exit(0)
 
+def connect_wifi():
+    wireless = Wireless()
+    wireless.connect(ssid='WIFIonICE', password=False)
+
+def wifi_login():
+    browser = RoboBrowser()
+    browser.open('http://login.wifionice.de/')
+    login_form = browser.get_form()
+    browser.submit_form(login_form)
+
+def get_current_quota():
+    usage = float(requests.get('http://login.wifionice.de/usage_info/').content)
+    return usage
+
 print("wifi on ice started, press Ctrl+C to exit.")
-driver = webdriver.Firefox(firefox_options=firefox_options)
-signal.signal(signal.SIGINT, signal_handler)
+
 while True:
-    driver.get('http://login.wifionice.de/de/')
     try:
-        sleep(2)
-        quota_now = driver.find_element(By.CLASS_NAME, 'progress-bar').get_attribute('aria-valuenow')
-        print('Currently {}% of your quota are used.'.format(quota_now))
+        time.sleep(2)
+        quota_now = get_current_quota()
+        print('Currently {0.2f}% of your quota are used.'.format(quota_now))
         if int(quota_now) > 90:
             print('More than 90% of your quota are used, reconnecting!!!')
-            os.system('sudo ip l set {} down'.format(args.interface))
-            os.system('sudo macchanger -e {}'.format(args.interface))
-            os.system('sudo ip l set {} up'.format(args.interface))
-    except NoSuchElementException:
-        driver.find_element(By.ID, 'connect').submit()
-        continue
+            os.system('sudo macchanger -r {}'.format(args.interface))
+            time.sleep(1)
+            connect_wifi()
+            time.sleep(2)
+            wifi_login()
+    except Exception, e:
+        print(e)
+        pass
+
     os.system('sudo true')  # keep sudo active
-    sleep(args.sleep)
+    time.sleep(args.sleep)
